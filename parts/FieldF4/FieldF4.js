371710089dd0ed2d2dc6c94f34c5190bbfaa19bb
fs-kashiwakura <y.kashiwakura@fairsystem.co.jp>
2024-11-11 09:34:34 +0900

    FieldF4に引き算割り算を実装

--- parts/FieldF4/FieldF4.js
+++ parts/FieldF4/FieldF4.js
@@ -16,6 +16,10 @@ export class FieldF4 {
 	  return new FieldF4(additionTable[this.value][other.value]);
 	}
   
+	subtract(other){
+		return this.add(other);
+	}
+
 	// 乗算 (3次巡回群)
 	multiply(other) {
 	  const multiplicationTable = [
@@ -27,6 +31,20 @@ export class FieldF4 {
 	  return new FieldF4(multiplicationTable[this.value][other.value]);
 	}
   
+	divide(other) {
+		// 0除算はエラー
+		if (other.value == 0) {
+			throw new Error("0除算はできません");
+		}
+		const divisionTable = [
+		  [undefined, 0, 0, 0],  // 0 / { 1, w, wb} = 0
+		  [undefined, 1, 2, 3],  // 1 / { 1, w, wb} = { 1, wb, w}
+		  [undefined, 2, 3, 1],  // w / { 1, w, wb} = { w, 1, wb}
+		  [undefined, 3, 1, 2]   // wb / { 1, w, wb} = { wb, w, 1}
+		];
+		return new FieldF4(divisionTable[this.value][other.value]);
+	  }
+
 	// 文字列表示
 	toString() {
 	  switch (this.value) {
